a1daeeb16af5afa27a3c7503f95a0f522f815580
brennoms <brennoms2004@gmail.com>
2025-04-28 18:02:34 -0300

    feat: configura arquivos de inicialização

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
-// App.jsx
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
