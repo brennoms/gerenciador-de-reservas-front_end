3f0feaab315a47618ddd58ca3f643e9b4fac4825
brennoms <brennoms2004@gmail.com>
2025-04-28 18:01:58 -0300

    feat: adiciona arquivo que configura as rotas da aplicação

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,1 +1,33 @@
-// AppRoutes.jsx
+import { Routes, Route } from 'react-router-dom';
+
+import ProtectedRoute from './ProtectedRoute';
+import PublicLayout from '../layouts/PublicLayout';
+import PrivateLayout from '../layouts/PrivateLayout';
+import Home from '../pages/Home';
+
+const privateRoutes = {
+  properties: {
+    path: '/properties',
+    element: (
+      <ProtectedRoute>
+        <p>properties</p>
+      </ProtectedRoute>
+    ),
+  },
+};
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<PublicLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<p>Login</p>} />
+        <Route path="/register" element={<p>Register</p>} />
+      </Route>
+
+      <Route element={<PrivateLayout />}>
+        <Route path={privateRoutes.properties.path} element={privateRoutes.properties.element} />
+      </Route>
+    </Routes>
+  );
+}
