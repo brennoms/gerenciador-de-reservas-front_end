56f149979d4d00b45fa4b4dcb3b3545aa5601530
brennoms <brennoms2004@gmail.com>
2025-04-28 17:42:20 -0300

    feat: adiciona um layout para paginas publicas

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,1 +1,21 @@
-// PublicLayout.jsx
+import { Outlet } from 'react-router-dom';
+
+import './PublicLayout.css';
+
+export default function PublicLayout() {
+  return (
+    <div>
+      <header>
+        <h1>Site de Imóveis</h1>
+      </header>
+
+      <main>
+        <Outlet /> {/* Aqui entram as páginas públicas */}
+      </main>
+
+      <footer>
+        <p>© 2025 - Todos os direitos reservados</p>
+      </footer>
+    </div>
+  );
+}
