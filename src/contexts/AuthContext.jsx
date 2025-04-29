e4e97731df8f50fbe137616c52ca36be0e2e1e6a
brennoms <brennoms2004@gmail.com>
2025-04-29 17:00:36 -0300

    feat: implementa serviços de autenticação com login e verificação de token

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
+
+import { loginService, verifyService } from '../services/authService';
 
 const AuthContext = createContext();
 
@@ -9,17 +11,43 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [token, setToken] = useState(null);
 
-  // Função de login (você pode chamar sua API aqui dentro)
-  function login(userData) {
-    setToken(userData);
-    // Aqui você pode salvar no localStorage se quiser persistir
+  useEffect(() => {
+    verifyStorageToken();
+  }, []);
+
+  async function verifyStorageToken() {
+    const storedToken = JSON.parse(localStorage.getItem('token'));
+    if (storedToken) {
+      const res = await verifyService(storedToken);
+      if (!res) {
+        localStorage.removeItem('token');
+        setToken(null);
+        return false;
+      } else {
+        setToken(storedToken);
+        return true;
+      }
+    }
   }
 
-  // Função de logout
+  async function login(userData) {
+    const res = await loginService(userData.email, userData.pass);
+    if (!res.token) {
+      return res.erro;
+    }
+    localStorage.setItem('token', JSON.stringify(res.token));
+    setToken(res.token);
+    return true;
+  }
+
   function logout() {
+    localStorage.removeItem('token');
     setToken(null);
-    // Também pode limpar o localStorage aqui
   }
 
-  return <AuthContext.Provider value={{ token, login, logout }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ token, login, logout, verifyStorageToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,26 @@
-// authService.js
+import api from './api';
+
+export async function loginService(email, senha) {
+  try {
+    const res = await api.post('/usuarios/login', { email, senha });
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+export async function verifyService(token) {
+  try {
+    const res = await api.get('/usuarios/me', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (res.status === 200) {
+      return true;
+    }
+    return false;
+  } catch {
+    return false;
+  }
+}
