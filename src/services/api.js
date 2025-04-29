87cfe59b3f1e94bebf21cd26b40b6bb13ad64858
brennoms <brennoms2004@gmail.com>
2025-04-29 14:54:39 -0300

    feat: implementa instancia axios para requisições a api

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,9 @@
-// api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 5000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default api;
