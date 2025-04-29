474bf71d1e8ec0e7238e18517e6dad7be2036c8a
brennoms <brennoms2004@gmail.com>
2025-04-29 11:46:26 -0300

    feat: atualiza a pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
+import '../styles/displays.css';
+import '../styles/buttons.css';
+
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+    <div className="flex column-center gap-1 min-height-80vh">
+      <button className="button1" onClick={() => navigate('/login')}>
+        Entrar
+      </button>
+      <button className="button1" onClick={() => navigate('/register')}>
+        Cadastrar
+      </button>
     </div>
   );
 }
