ab74a72c98eacb224752f4ad800298c02c9a7e30
brennoms <brennoms2004@gmail.com>
2025-04-30 18:13:50 -0300

    feat: implementa layout privado com sidebar interativa e navegação

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,13 +1,40 @@
-import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { Outlet, Link, useNavigate } from 'react-router-dom';
+import { Menu, X } from 'lucide-react';
+
+import { useAuth } from '../contexts/AuthContext';
 
 import './PrivateLayout.css';
 
 export default function PrivateLayout() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  function exit() {
+    navigate('/');
+    setTimeout(() => logout(), 500);
+  }
+
   return (
-    <div className="back">
-      <aside>
-        <nav></nav>
-      </aside>
+    <div className="flex">
+      <div className="div-sidebar">
+        <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+          <button type="button" onClick={() => setIsOpen(!isOpen)} className="toggle-btn">
+            {isOpen ? <X /> : <Menu />}
+          </button>
+          <div className={`div-links ${isOpen ? 'show' : 'hide'}`}>
+            <div className="links">
+              <li>🏠 exemplo1</li>
+              <li>📅 exemplo2</li>
+              <li>👤 exemplo3</li>
+            </div>
+            <button className="button1" type="button" onClick={exit}>
+              Sair
+            </button>
+          </div>
+        </div>
+      </div>
 
       <div>
         <main>
