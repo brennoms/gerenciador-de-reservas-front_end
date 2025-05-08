7aa20890e3a6b8942bc6033f11daca3bc64e443a
brennoms <brennoms2004@gmail.com>
2025-05-08 15:25:38 -0300

    feat: adiciona className ao componente Carrousel e atualiza os estilos de Property e Carrousel

--- src/pages/Property.jsx
+++ src/pages/Property.jsx
@@ -5,6 +5,7 @@ import { useAuth } from '../contexts/AuthContext';
 import { useSideBarContext } from '../layouts/PrivateLayout';
 import { getProperty } from '../services/propertiesService';
 import Carrousel from '../components/Carrousel';
+import MonthCalendar from '../components/MonthCalendar';
 
 export default function Property() {
   const { propertyId } = useParams();
@@ -24,33 +25,31 @@ export default function Property() {
   useEffect(() => {
     const list = [];
     for (let i = 1; i < 13; i++) {
-      list.push(
-        <div>
-          <p>mes {i}</p>
-        </div>
-      );
+      list.push(<MonthCalendar month={i} />);
     }
     setCalendars(list);
   }, [year]);
 
   return (
-    <div className="flex column-center">
-      {property ? (
-        <>
-          <h1 className="text-center">{property.name}</h1>
-          <img className="border-radius-1rem" src={property.imageUrl} />
-          <p className="marborpad0">{property.adress}</p>
-        </>
-      ) : (
-        ''
-      )}
-      <hr className="width90p" />
-      <p>{year}</p>
-      <div className="flex" style={{ width: '100%' }}>
-        <div style={{ width: '50%' }}>
+    <div className="max-w-full h-full pr-3">
+      <div className="flex flex-col items-center mb-2">
+        {property ? (
+          <>
+            <h1 className="default-h1 mb-0">{property.name}</h1>
+            <p className="text-gray-500">{property.adress}</p>
+            <img className="rounded w-11/12 sm:w-min" src={property.imageUrl} />
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      <hr className="border" />
+      <div className="flex flex-col sm:flex-row max-w-full max-h-fit p-2">
+        <div className="bg-red-500 w-full sm:w-1/2">
+          <p className="text-center">{year}</p>
           <Carrousel cards={calendars} initialCard={new Date().getMonth()} />
         </div>
-        <div style={{ width: '50%' }}>
+        <div className="bg-blue-500 w-full sm:w-1/2">
           <p>detalhes da reserva</p>
         </div>
       </div>
