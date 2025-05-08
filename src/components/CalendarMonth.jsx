197a5c99a871b2049c1101695fed912025057564
brennoms <brennoms2004@gmail.com>
2025-05-08 18:42:37 -0300

    feat: altera estilo de CalendarMonth

--- src/components/CalendarMonth.jsx
+++ src/components/CalendarMonth.jsx
@@ -1,12 +1,17 @@
+import { useRef } from 'react';
+
 export default function MonthCalendar({ month }) {
+  const CalendarRef = useRef(null);
+
   return (
     <div className="flex flex-col items-center">
       <p>{month.name}</p>
-      <div className="grid grid-cols-7 gap-1">
+      <div className="grid grid-cols-7 gap-1" ref={CalendarRef}>
         {month.days.map(day => (
           <button
-            className="rounded aspect-square bg-white p-1 border border-black "
+            className={`rounded aspect-square bg-white p-1 border border-black`}
             key={day.number}
+            type="button"
           >
             {day.number}
           </button>
