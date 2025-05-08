f435a1f1218eb29c5dc566a932ec17b5372e8989
brennoms <brennoms2004@gmail.com>
2025-05-08 09:53:28 -0300

    feat: configura arquivos para usar tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
-import './styles/global.jsx';
 
 function App() {
   return (
